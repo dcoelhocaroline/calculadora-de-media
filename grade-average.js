820ec164564040c13f315be77023e5fabdbdc4f8
dcoelhocaroline <dcoelhocaroline@gmail.com>
2021-09-15 20:01:24 -0300

    Correção de bugs

--- grade-average.js
+++ grade-average.js
@@ -5,14 +5,17 @@ function calcAverage(){
     var grade3 = parseFloat(document.getElementById("grade3").value);
     var grade4 = parseFloat(document.getElementById("grade4").value);
  
-    var average = (grade1 + grade2 + grade3 + grade4) / 4;
+    var average = Number((grade1 + grade2 + grade3 + grade4) / 4);
 
-    document.getElementById("gradeAverage").innerHTML = average.toFixed(2)
     
-    if (average >= 7) {
-        document.getElementById("gradeInfo").innerHTML = "Você passou!"
-    }else {
-        document.getElementById("gradeInfo").innerHTML = "Você não atingiu a média."
+        document.getElementById("gradeAverage").innerHTML = average.toFixed(1)
+        
+        if (average >= 7 && average <= 10) {
+            document.getElementById("gradeInfo").innerHTML = "Você passou!"
+        } else if (average < 7 && average >= 0){
+            document.getElementById("gradeInfo").innerHTML = "Você não atingiu a média."
+        } else {
+            document.getElementById("gradeAverage").innerHTML = ""
+            document.getElementById("gradeInfo").innerHTML = "Parece que tivemos um erro.<br>Verifique se inseriu as notas corretamente."
         }
-
 }
